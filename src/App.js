da24f180f81d80194183b19b17815d2e1fbc9c2a
AmandaTodero <amanda.todero032@gmail.com>
2024-01-02 14:37:13 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 export default function App() {
   return <div className="App">
+    <div className="container">
     <h1> Weather App </h1>
     <footer>
       This project was coded by Amanda Todero and is  
     <a href="https://github.com/AmandaTodero3/react-weather-app" target="_blank">   open-sourced
     </a>
     </footer>
+    </div>
     </div>
 }
