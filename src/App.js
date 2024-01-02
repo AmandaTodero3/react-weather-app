d3770224c439e21cba42935d884dc023421043df
AmandaTodero <amanda.todero032@gmail.com>
2024-01-02 13:41:31 -0500

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from "react";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    
+export default function App() {
+  return <div className="App">
+    <h1> Weather App </h1>
+    <footer>
+      This project was coded by Amanda Todero and is  
+    <a href="https://github.com/AmandaTodero3/react-weather-app" target="_blank">   open-sourced
+    </a>
+    </footer>
     </div>
-  );
 }
-
-export default App;
