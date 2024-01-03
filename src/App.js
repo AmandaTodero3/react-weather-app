aec6054bec6c458ffff8eb12a63e432bdb2fd6bb
AmandaTodero <amanda.todero032@gmail.com>
2024-01-03 15:38:54 -0500

    Added CSS

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 
 export default function App() {
-  return <div className="App">
-    <div className="container">
-    <h1> Weather App </h1>
-    <Weather/>
-    <footer>
-      This project was coded by Amanda Todero and is  
-    <a href="https://github.com/AmandaTodero3/react-weather-app" target="_blank">   open-sourced
-    </a>
-    </footer>
+  return (
+    <div className="App">
+      <div className="container">
+        <Weather />
+        <footer>
+          This project was coded by Amanda Todero and is
+          <a
+            href="https://github.com/AmandaTodero3/react-weather-app"
+            target="_blank"
+          >
+            {" "}
+            open-sourced
+          </a>
+        </footer>
+      </div>
     </div>
-    </div>
+  );
 }
