6023f3fd63db7a10aa909da3895330b7ca692ee3
AmandaTodero <amanda.todero032@gmail.com>
2024-01-08 09:33:19 -0500

    fixed icon url

--- src/Weather.js
+++ src/Weather.js
@@ -14,7 +14,7 @@ export default function Weather(props) {
       humidity: response.data.main.humidity,
       wind: response.data.wind.speed,
       description: response.data.weather[0].description,
-      iconUrl: `https://openweathermap.org/img/wn/${response.weather.data[0].icon}`,
+      iconUrl: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
       date: new Date(response.data.dt * 1000),
       city: response.data.name,
     });
